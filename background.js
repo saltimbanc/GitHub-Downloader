655f0a2379cd7305fae50e3f7f2c61b5c6f028b7
Titus <titus@mac.local>
2020-01-22 18:39:43 +0200

    Save files with the correct extension

--- background.js
+++ background.js
@@ -1,8 +1,32 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if(request.cmd == "download"){
-    chrome.downloads.download({
-      url: request.url,
-      filename: decodeURIComponent(request.url.match(/(?!\/)[^\/]+$/)[0])
+    const { url } = request; 
+    const filename = decodeURIComponent(url.match(/(?!\/)[^\/]+$/).shift());
+    const extension = filename.replace(/.*\.(?=[^.]+$)/, "");
+    const mime = EXTENSION_TO_MIME[extension] || `text/${extension}`;
+
+    fetch(request.url, {
+      mode: "cors",
+      credentials: "include"
+    }).then(response => response.blob())
+      .then(blob => {
+        return blob.slice(0, blob.size, mime);
+      }).then((blob) => {
+        chrome.downloads.download({ url: URL.createObjectURL(blob), filename });
+      });
+  }
+});
+
+chrome.downloads.onChanged.addListener((changes) => {
+  if (changes && changes.state && changes.state.current === "complete") {
+    chrome.downloads.search({
+      id: changes.id
+    }, (results) => {
+      results.forEach((downloadItem) => {
+        if (downloadItem.byExtensionId === chrome.runtime.id && downloadItem.url.startsWith("blob:")) {
+          URL.revokeObjectURL(downloadItem.url);
+        }
+      });
     });
-	}
+  }
 });
